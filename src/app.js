c5d97e9780b5f4b8f5e7540519bdd93c0514d227
admin <hypermail@yandex.ru>
2022-02-23 22:59:41 +0300

    disable api load if user search are null

--- src/app.js
+++ src/app.js
@@ -3,10 +3,12 @@ import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/js/bootstrap.js';
 import $ from 'jquery'
 import logo from './img/bitcoin_logo.png'
-import address_endpoint from './js/api.js'
+import addressEndpoint from './js/api.js'
 import arrayAssembler from './js/array_assembler'
 
-address_endpoint.then(data => {
+const address_endpoint = addressEndpoint();
+
+address_endpoint.then(data => { //тут обработать ошибку Uncaught TypeError: Cannot read properties of undefined (reading 'then')
 	const a = arrayAssembler(data);
     console.log(a)
 })
--- src/js/api.js
+++ src/js/api.js
@@ -1,27 +1,28 @@
 //TODO обработать ошибку неправильного ввода
 //TODO сделать так чтобы при заходе на index.html автоматически не отправлялся запрос
 //get user data from search
-window.addEventListener('load', getUserInput)
 
-function getUserInput() {
-    const params = (new URL(document.location)).searchParams
-    const user_search = params.get('user_search')
-    return user_search
+export default function addressEndpoint() {
+
+    window.addEventListener('load', getUserInput)
+
+    function getUserInput() {
+        const params = (new URL(document.location)).searchParams
+        const user_search = params.get('user_search')
+        return user_search
+    }
+
+    const user_search = getUserInput()
+
+    if (user_search != null) {
+
+        const address_endpoint = new Promise(async function(resolve, reject){
+            const response = await fetch('https://api.blockcypher.com/v1/btc/main/addrs/' + [user_search] + '?limit=9999')
+            const data = await response.json()
+            resolve(data)
+        })
+        return address_endpoint //https://www.blockcypher.com/dev/bitcoin/#address-endpoint
+    }
+
 }
-
-const user_search = getUserInput()
-
-const address_endpoint_url = 'https://api.blockcypher.com/v1/btc/main/addrs/' + [user_search] + '?limit=9999'
-
-//get transaction array from server
-function addressEndpoint(method, address_endpoint_url) {
-    return fetch(address_endpoint_url).then(response => {
-        return response.json()
-    })
-}
-
-const address_endpoint = addressEndpoint('GET', address_endpoint_url)
-
-export default address_endpoint //https://www.blockcypher.com/dev/bitcoin/#address-endpoint
-
 
