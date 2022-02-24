8a4698449800ece277e1b75ceff245c3b5b25c4f
admin <hypermail@yandex.ru>
2022-02-24 11:26:48 +0300

    rebuild api request, request by click

--- src/app.js
+++ src/app.js
@@ -3,10 +3,20 @@ import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/js/bootstrap.js';
 import $ from 'jquery'
 import logo from './img/bitcoin_logo.png'
-import { address_endpoint } from './js/api.js'
+import { addressEndpoint } from './js/api.js'
 import arrayAssembler from './js/array_assembler'
 
-address_endpoint.then(data => { //тут обработать ошибку Uncaught TypeError: Cannot read properties of undefined (reading 'then')
-    const log = arrayAssembler(data)
-    console.log(log)
-})
+// const address_endpoint = addressEndpoint();
+
+// function someFunc (address_endpoint) {
+    
+//     address_endpoint.then(data => { //тут обработать ошибку Uncaught TypeError: Cannot read properties of undefined (reading 'then')
+//         const log = arrayAssembler(data)
+//         console.log(log)
+//     })
+
+// }
+
+
+
+
--- src/js/api.js
+++ src/js/api.js
@@ -1,26 +1,56 @@
 //TODO обработать ошибку неправильного ввода
 //TODO сделать так чтобы при заходе на index.html автоматически не отправлялся запрос
 //get user data from search
+import arrayAssembler from './array_assembler'
 
 function addressEndpoint() {
 
-    window.addEventListener('load', getUserInput)
+    const form = document.getElementById('form')
 
-    function getUserInput() {
-        const params = (new URL(document.location)).searchParams
-        const user_search = params.get('user_search')
-        return user_search
-    }
+    form.addEventListener('submit', function a(e) {
+        e.preventDefault();
+        var data = new FormData(this),
+            user_search = data.get('user_search')
+        console.log(user_search)
+        if (user_search != null) {
+            const address_endpoint = new Promise(async function (resolve, reject) {
+                const response = await fetch('https://api.blockcypher.com/v1/btc/main/addrs/' + [user_search] + '?limit=9999')
+                const data = await response.json()
+                resolve(data)
+            })
+            address_endpoint.then(data => { //тут обработать ошибку Uncaught TypeError: Cannot read properties of undefined (reading 'then')
+                const final_array = arrayAssembler(data)
+                console.log(final_array)
+            })
+        }
 
-    const user_search = getUserInput()
+    })
 
-    if (user_search != null) {
-        const address_endpoint = new Promise(async function (resolve, reject) {
-            const response = await fetch('https://api.blockcypher.com/v1/btc/main/addrs/' + [user_search] + '?limit=9999')
-            const data = await response.json()
-            resolve(data)
-        })
-        return address_endpoint //https://www.blockcypher.com/dev/bitcoin/#address-endpoint
-    }
 }
-export const address_endpoint = addressEndpoint()
+
+
+
+
+window.addressEndpoint = addressEndpoint
+
+// window.addEventListener('load', getUserInput)
+
+// function getUserInput() {
+//     const params = (new URL(document.location)).searchParams
+//     const user_search = params.get('user_search')
+//     return user_search
+// }
+
+// const user_search = someFunc()
+// console.log(user_search)
+
+// if (user_search != null) {
+//     const address_endpoint = new Promise(async function (resolve, reject) {
+//         const response = await fetch('https://api.blockcypher.com/v1/btc/main/addrs/' + [user_search] + '?limit=9999')
+//         const data = await response.json()
+//         resolve(data)
+//     })
+// }
+
+// export {someFunc}
+
